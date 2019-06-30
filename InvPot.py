1f66afaa29f15ce8b1e8a1a1886a3721fb910001
Alberto Rigotto <alberto.rigotto@blindside.pro>
2019-06-30 18:45:08 -0300

    Fixed inverted pot method

--- InvPot.py
+++ InvPot.py
@@ -4,12 +4,19 @@ import numpy as np
 def inverted_potency(A, tolerance, X):
     lambda_new = 0
     x_old = X/np.linalg.norm(X)
+    A_inv = np.linalg.inv(A)
+    print('A inverse', A_inv)
     while True:
         lambda_old = lambda_new
-        y_new = np.invert(A).dot(x_old)
+        y_new = A_inv.dot(x_old)
         x_new = y_new/np.linalg.norm(y_new)
         lambda_new = np.transpose(x_old).dot(y_new)
+        print('x_new\n',  x_new)
+        print('lambda_new', lambda_new)
+        print('lambda_old', lambda_old)
         x_old = x_new
-        if lambda_new - lambda_old/lambda_new < tolerance:
+        if (lambda_new - lambda_old)/lambda_new < tolerance:
+            print('convergiu!')
+            print('lambda final:', 1/lambda_new)
             break
     return 1/lambda_new, x_new
